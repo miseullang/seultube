97aeebc7fd3653cd847a2f1dff3bfa5dc573b1df
슬 <seul@seul-ui-iMac.local>
2024-02-25 00:49:12 +0900

    컴포넌트 비동기 작업

--- src/pages/Webdesign.jsx
+++ src/pages/Webdesign.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Main from '../components/section/Main'
 
 const Webdesign = () => {
   return (
-    <div>
+    <Main>
       Webdesign
-    </div>
+    </Main>
   )
 }
 
