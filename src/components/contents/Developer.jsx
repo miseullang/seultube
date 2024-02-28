926018d17bbe2f57db1745e0b8b06f2269cfe617
슬 <seul@seul-ui-iMac.local>
2024-02-29 01:42:41 +0900

    추천 개발자 파트 작업

--- src/components/contents/Developer.jsx
+++ src/components/contents/Developer.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
+import { developerText } from '../../data/developer'
+import { Link } from 'react-router-dom'
+
 const Developer = () => {
   return (
-    <div>
-      Developer
-    </div>
+    <section id='developer'>
+      <h2>추천 개발자를 소개합니다.</h2>
+      <div className='developer_inner overflow'>
+        {developerText.map((developer, key) => (
+          <div className='developer' key={key}>
+            <div className="developer_img play_icon">
+              <Link to={`/channel/${developer.channelId}`}>
+                <img src={developer.img} alt={developer.name}></img>
+              </Link>
+            </div>
+            <div className="developer_info">
+              <Link to={`/channel/${developer.channelId}`}>
+                {developer.name}
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
